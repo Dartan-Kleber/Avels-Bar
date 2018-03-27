708e2999ddcc182859e08bc21851f0bf3f069788
Dartan-Kleber <markuss.mkb@gmail.com>
2018-03-27 14:13:53 -0300

    Criação do arquivo de estilo.css e inicil do arquivo index.php

---
+++ _html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="../_css/estilo.css"/>
+    <meta charset="UTF-8"/>
+    <title>Controle de Caixa - <?php echo date("d/m/Y")?></title>
+</head>
+<body>
+  <div>
+
+  </div>
+</body>
+</html>
+
